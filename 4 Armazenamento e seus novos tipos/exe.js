7cd7019088bcd61180b2416ed8a198b1efbabb4f
Wesley Alexandre <wesley.lopes14@hotmail.com>
2024-01-03 16:23:12 -0300

    Finalizando Aula

---
+++ 4 Armazenamento e seus novos tipos/exe.js
@@ -0,0 +1,31 @@
+/* Utilizando método padrão
+
+Desvantagens de utilizar Array padrão, demora muito, se tiver uma lista grande armazenada
+
+const lista = ["arroz", "feijao", "peru"];
+const collection = [
+    {id: "123", name: "Wesley"},
+    {id: "321", name: "Pedro"}
+];
+
+const user = {name: "Ayrton"};
+
+console.log(user.name);
+*/
+
+/* Utilizando objetos
+
+Desvantagens: não possui todas as funções que o ARRAY tem
+Objetos não guarda a ordem que foi salva em objetos
+Você não sabe quantos elementos tem, apenas de forma manual
+
+
+*/
+
+
+const users = {
+    "123": {id: "123", name: "Wesley"},
+    "321": {id: "321", name: "Pedro"}
+};
+
+console.log(users["123"]);
